8fd5fd29592d3c5fcc3c16a4e125cb1a0b7ebfa3
asifiicl <asifiicl@gmail.com>
2022-04-25 10:19:04 +0530

    adding files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome <code>To</code> Gyancs Tutorial Points.
+        </p>
+        <a
+          className="App-link"
+          href="https://gyancs.in"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Gyancs.in
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
